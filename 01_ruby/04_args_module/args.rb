fbd080993d571febb667382a08893ca356dff382
ukyoda <black12172001@hotmail.co.jp>
2021-02-22 20:44:30 +0900

    コマンド引数の課題完了。 もっと良いコードかけそうな気がする・・・

--- 01_ruby/04_args_module/args.rb
+++ 01_ruby/04_args_module/args.rb
@@ -15,7 +15,8 @@
 # 3.
 #   2つのキーワード引数benriとsugoiを取るメソッド、 `configure` を作成してください
 #   また、2つのキーワード引数の他に、オプショナルでキーワード引数を取得できるようにしてください
-#   オプショナルなキーワード引数のうち、 `c_` で始まるキーワードが渡された場合には、benriとsugoiと同様の挙動をしてください。ただし、キーワードを扱うときに `c_` の文字列は取り除いてください。
+#   オプショナルなキーワード引数のうち、 `c_` で始まるキーワードが渡された場合には、benriとsugoiと同様の挙動をしてください。
+#   ただし、キーワードを扱うときに `c_` の文字列は取り除いてください。
 #   `c_` で始まるオプショナルなキーワードを扱うときは、文字列でソートした順に扱ってください。
 #   benriとyabaiの内容を、 "Configure 'keyword' as 'value'" の文字列で出力してください。keywordはキーワード、valueはキーワードに渡された値でそれぞれ埋めてください。
 #   `c_` で始まるキーワードに関しては、この二つの後に出力してください
@@ -26,3 +27,32 @@
 #         Configure 'sugoi' as 'oniyabai'
 #         Configure 'foo' as 'iihanashi'
 #         Configure 'hoge' as 'uhyo-'
+class Args
+    def calc(arg1, arg2, arg3=arg2)
+        val = arg1.to_s * arg2
+        res = []
+        arg3.times { |v| res.push(val)}
+        res
+    end
+
+    def count_object(array: [], keyword:)
+        array.inject(0) do |res, v|
+            res += v == keyword ? 1 : 0
+        end
+    end
+
+    def configure(benri:, sugoi:, **kwargs)
+        res = [
+            "Configure 'benri' as '#{benri}'",
+            "Configure 'sugoi' as '#{sugoi}'",
+        ]
+        kwargs = kwargs.filter do |k, v|
+            /^c_.*/.match(k.to_s)
+        end
+        keys = kwargs.keys().sort()
+        keys.each do |k|
+            res.push("Configure '#{k[2, k.length]}' as '#{kwargs[k]}'")
+        end
+        puts res.join "\n"
+    end
+end
